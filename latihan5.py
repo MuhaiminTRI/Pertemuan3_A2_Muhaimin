395b2bc4d83714e2cfe879a42622d578cf754ca4
MuhaiminTRI <142768777+MuhaiminTRI@users.noreply.github.com>
2023-08-28 09:47:05 +0700

    Add files via upload

---
+++ latihan5.py
@@ -0,0 +1,14 @@
+blog='Klinik' + 'Python'
+print(blog)
+newblog=blog*5
+print(newblog)
+blog*=4
+print(blog)
+string='Klinik''Python'
+print(string)
+buah='Nanas'
+buah[0]
+buah[0:2]
+buah[0:4]
+buah[0:5]
+print(buah)
